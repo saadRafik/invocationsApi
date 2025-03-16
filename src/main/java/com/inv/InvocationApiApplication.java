f12ff26f8c23963ec479c54722a677b6d879faaf
Saad <saadrafik75@gmail.com>
2025-03-17 00:31:40 +0100

    unniversal files

---
+++ src/main/java/com/inv/InvocationApiApplication.java
@@ -0,0 +1,11 @@
+package com.inv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvocationApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InvocationApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/inv/invocation_api/InvocationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.invocation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvocationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
